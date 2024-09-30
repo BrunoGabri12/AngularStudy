e4de82f0779309971d0bb0737c3a96ce7877c4dc
Bruno Santos <bruno.santos@oreons.com>
2024-09-29 22:35:49 -0300

    create hero page

---
+++ src/app/firts-page/firts-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firts-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './firts-page.component.html',
+  styleUrl: './firts-page.component.css'
+})
+export class FirtsPageComponent {
+
+}
