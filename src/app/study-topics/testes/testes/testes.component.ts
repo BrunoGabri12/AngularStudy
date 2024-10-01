ac76fa72540c67ad57122f4ac32f4eb1f84012c7
Bruno Santos <bruno.santos@oreons.com>
2024-10-01 10:55:17 -0300

    Feat: Create tests page

---
+++ src/app/study-topics/testes/testes/testes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testes',
+  standalone: true,
+  imports: [],
+  templateUrl: './testes.component.html',
+  styleUrl: './testes.component.css'
+})
+export class TestesComponent {
+
+}
